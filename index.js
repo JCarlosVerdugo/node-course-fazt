d3fd8e4a8da3013d9110666217c7725b9beadc54
Carlos Verdugo <carlosverdugo@proton.me>
2024-02-11 20:14:16 -0700

    Use timers

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-console.log('Hello world')
+// let username = 'fazt';
+// let hasHobbies = true;
+// let points = [10, 20, 30];
+// let user = {
+//     name: 'ryan',
+//     lastname: 'ray'
+// }
+// const PI = 3.1415;
+
+
+// const age = 18;
+
+// if (age >= 18) {
+//     console.log('you are an adult')
+// } else if (age >= 13) {
+//     console.log('you are a teen')
+// } else {
+//     console.log('you are a child')
+// }
+
+// const names = ['joe', 'john', 'marco'];
+
+// for (let i = 0; i < names.length; i++) {
+//     console.log(names[i])
+// }
+
+function showUserInfo(userName, userAge) {
+    return `The username is ${userName}, the user is ${userAge} years old`;
+}
+
+const user = showUserInfo('Maria', 32)
+console.log(user)
---
+++ timers.js
@@ -0,0 +1,10 @@
+// setInterval
+// setTimeOut
+
+// setInterval(() => {
+//     console.log('Hello world')
+// }, 1000);
+
+// setTimeout(() => {
+//     console.log('Hello world');
+// }, 5000);
