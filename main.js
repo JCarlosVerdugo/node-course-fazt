70373faae68719a238066ba1c178c490f3d9679a
Carlos Verdugo <carlosverdugo@proton.me>
2024-02-11 20:49:05 -0700

    Modules imports

---
+++ main.js
@@ -0,0 +1,5 @@
+const {myArray, myNumber, myWebAddress} = require('./module/myModule');
+
+console.log(myArray);
+console.log(myNumber);
+console.log(myWebAddress);
---
+++ module/myModule.js
@@ -0,0 +1,14 @@
+const myWebAddress = 'faztweb.com';
+const myNumber = 30;
+const myArray = [10, 20, 30];
+const user = {
+    name: 'john',
+    lastname: 'doe'
+}; 
+
+module.exports = {
+    myWebAddress,
+    myNumber,
+    myArray,
+    user
+}
